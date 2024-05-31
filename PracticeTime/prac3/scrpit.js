9bc484e1291e599265ab9719ff004841436333d9
AbrarUr-Rahman <ishmam000@gmail.com>
2024-05-31 21:25:13 +0600

    remove practice from git

--- PracticeTime/prac3/scrpit.js
+++
@@ -1,9 +0,0 @@
-function toggleContent(element) {
-  const content = element.nextElementSibling;
-
-  if (content.style.display === "block") {
-    content.style.display = "none";
-  } else {
-    content.style.display = "block";
-  }
-}
