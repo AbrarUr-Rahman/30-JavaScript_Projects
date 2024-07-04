340263c793ae03d870d3b50e350f07e446bc917a
AbrarUr-Rahman <ishmam000@gmail.com>
2024-07-04 22:45:12 +0600

    Final Commit

--- SelectMenu/script.js
+++ SelectMenu/script.js
@@ -2,14 +2,17 @@ let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
 let list = document.getElementById("list");
+let arrowIcon = document.getElementById("arrowIcon");
 
 selectField.onclick = function () {
   list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate"); //!Toggle Property
 };
 
 for (option of options) {
   option.onclick = function () {
     selectText.innerHTML = this.innerText;
-    list.classList.toggle("hide");
+    list.classList.toggle("hide"); //!Toggle Property
+    arrowIcon.classList.toggle("rotate");
   };
 }
